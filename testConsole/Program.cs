7c19c91b2b6040ebe571e5c58ec7de3f81aeaab9
Nexos Marius <neskon@gmail.com>
2021-02-16 01:50:35 +0100

    initial commit

---
+++ testConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("testing jenkins pipeline .net core app !");
+        }
+    }
+}
