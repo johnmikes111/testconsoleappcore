fd0cee18974140b65df342fdcdba0d13da403457
johnmikes111 <51159859+johnmikes111@users.noreply.github.com>
2021-02-16 02:59:17 +0100

    Promena na github

--- testConsole/Program.cs
+++ testConsole/Program.cs
@@ -6,7 +6,7 @@ namespace testConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("testing jenkins pipeline .net core app neka izmena 333 sa readkey !");
+            Console.WriteLine("testing jenkins pipeline .net core app sada sam promenio na Github-u !");
             Console.ReadKey();
         }
     }
