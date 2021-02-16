5d5cbb16bc65c74baf109ce6dff101633c73fb3d
johnmikes111 <51159859+johnmikes111@users.noreply.github.com>
2021-02-16 03:22:19 +0100

    333

--- testConsole/Program.cs
+++ testConsole/Program.cs
@@ -6,7 +6,7 @@ namespace testConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("testing jenkins pipeline .net core app sada sam promenio na Github-u eeee !");
+            Console.WriteLine("testing jenkins pipeline .net core app sada sam promenio na Github-u e333eee !");
             Console.ReadKey();
         }
     }
